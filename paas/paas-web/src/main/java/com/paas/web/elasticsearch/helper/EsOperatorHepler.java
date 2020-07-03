2d67e8d3751037050cb0cf62bab3ce402ed7bca6
lonelyCoder-jrbing <719203729@qq.com>
2020-07-03 19:11:29 +0800

    1.es配置

---
+++ paas/paas-web/src/main/java/com/paas/web/elasticsearch/helper/EsOperatorHepler.java
@@ -0,0 +1,21 @@
+package com.paas.web.elasticsearch.helper;
+
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * create by sumerian on 2020/7/3
+ * <p>
+ * desc:es增删改查
+ **/
+public class EsOperatorHepler {
+
+    @Autowired
+    RestHighLevelClient clients;
+
+
+
+
+
+
+}
