b96c3c9722a351efb16bfd08521ffab945632b21
lonelyCoder-jrbing <719203729@qq.com>
2020-07-07 13:13:48 +0800

    全局异常的处理，添加了常见的一些运行时的异常

--- paas/paas-web/src/main/java/com/paas/web/conf/DispatcherRegistrationConfig.java
+++ paas/paas-web/src/main/java/com/paas/web/conf/DispatcherRegistrationConfig.java
@@ -1,12 +1,14 @@
 package com.paas.web.conf;
 
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
 /**
  * create by sumerian on 2020/6/18
  * <p>
  * desc:  dispatcher 配置 ServerletRegistrationBean
  **/
 public class DispatcherRegistrationConfig {
-
 
 
 }
